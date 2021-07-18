85d9b936c74677f666b84a7ffaddd0e5fcf6c915
Ederson Ferreira <ederson.dev@gmail.com>
2021-07-18 19:24:32 -0300

    Start project ms-territory

---
+++ src/main/java/com/edersonferreira/msterritory/MsTerritoryApplication.java
@@ -0,0 +1,13 @@
+package com.edersonferreira.msterritory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTerritoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTerritoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edersonferreira/msterritory/MsTerritoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edersonferreira.msterritory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTerritoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
